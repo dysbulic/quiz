03c08dc3c2fd12c5c48658a9856efe2f8d63fa48
dysbulic <dys@dhappy.org>
2023-07-12 14:25:26 -0400

    fixin' tooltips, formattin' questions, & (de)muxin' state 🦷

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
+import { ReactElement, useState } from 'react'
 import { z } from 'zod'
+import { Tooltip } from 'react-tooltip'
 import { sequence, shuffle } from './utils'
 import form from './data/Form M.json5'
 import './App.css'
-import { Tooltip, TooltipContent, TooltipTrigger } from './Tooltip'
+import { renderToStaticMarkup } from 'react-dom/server'
 
 type Maybe<T> = T | null
 
+type Question = {
+  question: string
+  options: Array<string>
+}
 type Questions = {
   part: string
-  questions: Array<{
-    question: string
-    options: Array<string>
-  }>
+  questions: Array<Question>
 }
 const QuestionsSchema = z.object({
   part: z.string(),
   questions: z.array(z.object({
     question: z.string(),
-    options: z.array(z.string()),
+    options: z.array(z.string()).length(2),
   }))
 })
 
+type Pair = Array<string>
 type Pairs = {
   part: string
-  pairs: Array<Array<string>>
+  pairs: Array<Pair>
 }
 const PairsSchema = z.object({
   part: z.string(),
-  pairs: z.array(z.array(z.string())),
+  pairs: z.array(z.array(z.string()).length(2)),
 })
 
 const ConfigSchema = (
@@ -37,97 +41,238 @@ const ConfigSchema = (
   ]))
 )
 
-export function App() {
-  const sections = ConfigSchema.parse(form)
-  const groups = sections.map((sec) => (
-    (sec as Questions).questions
-    ?? (sec as Pairs).pairs
-  ))
-  const choices = groups.flat()
-  const params = (
-    new URLSearchParams(window.location.search)
-  )
-  const seq = shuffle<number>(
-    sequence(choices.length),
-    { seed: params.get('seed') ?? undefined },
-  )
-  const state = params.get('state')
-  let bits: Maybe<Array<string>> = null
-  if(state) {
-    try {
-      bits = Array.from(
-        atob(state)
-        .split('')
-        .map((char) => char.charCodeAt(0))
-        .map((byte) => (
-          byte.toString(2).padStart(8, '0')
-        ))
-        .join('')
-        .match(/../g)
-        ?? []
-      )
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-      while(bits.length > 0 && bits.at(-1) === '00') {
-        bits.pop()
-      }
-      if(bits.length === 0) {
-        bits = null
-      }
-    } catch(err) {
-      console.error((err as Error).message)
+type URLParams = {
+  index?: string
+  state?: string
+  seed?: string
+}
+
+const state2progress = (state?: Maybe<string>) => {
+  if(state == null) return []
+
+  try {
+    let bits: Maybe<Array<string>> = Array.from(
+      atob(state)
+      .split('')
+      .map((char) => char.charCodeAt(0))
+      .map((byte) => (
+        byte.toString(2).padStart(8, '0')
+      ))
+      .join('')
+      .match(/../g)
+      ?? []
+    )
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    while(bits.length > 0 && bits.at(-1) === '00') {
+      bits.pop()
     }
-  }
-  const progress = (
-    bits?.map((field) => {
+    if(bits.length === 0) {
+      bits = []
+    }
+
+    return bits.map((field) => {
       switch(field) {
         case '01': return 0
         case '10': return 1
         default: return null
       }
     })
+  } catch(err) {
+    console.error((err as Error).message)
+  }
+
+  return null
+}
+
+const progress2state = (
+  (progress?: Maybe<Array<Maybe<number>>>) => {
+    if(progress == null) return null
+
+    try {
+      const bits = progress.map((field) => {
+        switch(field) {
+          case 0: return '01'
+          case 1: return '10'
+          default: return '00'
+        }
+      })
+      const state = (
+        bits
+        .join('')
+        .match(/.{1,8}/g)
+        ?.map((byte) => (
+          String.fromCharCode(parseInt(byte, 2))
+        ))
+        .join('')
+      ) as string
+      const out = btoa(state)
+
+      return out || null
+    } catch(err) {
+      console.error((err as Error).message)
+    }
+
+    return null
+  }
+)
+
+export function App() {
+  const params = (
+    new URLSearchParams(window.location.search)
+  )
+  const qIndexString = params.get('index')
+  const qIndex = (
+    qIndexString ? Number(qIndexString) - 1 : null
+  )
+  const sections = ConfigSchema.parse(form)
+  const groups = sections.map((sec) => (
+    (sec as Questions).questions
+    ?? (sec as Pairs).pairs
+  ))
+  const choices = groups.flat()
+  const seed = params.get('seed') ?? undefined
+  const seq = shuffle<number>(
+    sequence(choices.length), { seed },
+  )
+  const state = params.get('state')
+  const [progress, setProgress] = (
+    useState(state2progress(state))
   )
 
   const getChoice = (
     { idx, choice }:
-    { idx: number, choice: Maybe<number> }
+    { idx: number, choice?: Maybe<number> }
   ) => {
-    return `Question #${idx + 1} (${choice ?? '?'})`
+    if(idx >= choices.length) {
+      throw new Error('Out of bounds.')
+    }
+
+    const entry = choices[idx]
+    const question = (
+      <section>
+        {(() => {
+          if(entry.question) {
+            if(choice == null) {
+              return `${entry.question} ${entry.options.join(' or ')}?`
+            } else {
+              return (
+                <p>
+                  {entry.question}
+                  {' '}
+                  <span className="chosen">
+                    {entry.options[choice]}
+                  </span>
+                  {' or '}
+                  {entry.options[(choice + 1) % 2]}
+                  {'?'}
+                </p>
+              )
+            }
+          } else if(Array.isArray(entry)) {
+            if(choice == null) {
+              return `${entry.join(' or ')}?`
+            } else {
+              return (
+                <>
+                  <span className="chosen">
+                    {entry[choice]}
+                  </span>
+                  {' or '}
+                  {entry[(choice + 1) % 2]}
+                  {'?'}
+                </>
+              )
+            }
+          } else {
+            return (
+              `Unknown type of entry: "${
+                JSON.stringify(entry, null, 2)
+              }".`
+            )
+          }
+        })()}
+      </section>
+    )
+    return { question, entry }
   }
-  
+  const currentIndex = Math.min(
+    choices.length - 1, qIndex ?? progress?.length ?? 0
+  )
+
   return (
     <main>
       <h1>MBTI Inventory</h1>
 
       <ol id="progress">
-        {progress?.map(
-          (choice: Maybe<number>, idx: number) => {
+        {choices.map(
+          (_entry: Pair | Question, idx: number) => {
             if(idx >= seq.length) return null
 
+            const selection = seq[idx]
+            const choice = progress?.[idx]
             const known = choice != null
-            const q = getChoice(
-              { idx: seq[idx], choice }
+
+            const { question: q } = getChoice(
+              { idx: selection, choice }
             )
+
+            const linkParams: URLParams = {
+              index: String(idx + 1),
+            }
+            Object.entries(
+              { state: progress2state(progress), seed }
+            ).map(([key, value]) => {
+              if(value) {
+                linkParams[key as keyof URLParams] = (
+                  value
+                )
+              }
+            })
+            const query = new URLSearchParams(linkParams)
+
+            const classes = (
+              [`${!known ? 'un' : ''}known`]
+            )
+            if(idx === currentIndex) {
+              classes.push('active')
+            }
+
             return (
-              <Tooltip key={idx}>
-                <TooltipTrigger>
-                  <li
-                    className={
-                      known ? 'known' : 'unknown'
-                    }
-                  >
-                    {idx + 1}
-                  </li>
-                </TooltipTrigger>
-                <TooltipContent className="tooltip">
-                  {q}
-                </TooltipContent>
-              </Tooltip>
+              <li
+                key={idx}
+                className={classes.join(' ')}
+                data-tooltip-id="app-tooltip"
+                data-tooltip-html={
+                  renderToStaticMarkup(q)
+                }
+              >
+                <a href={`?${query.toString()}`}>
+                  {idx + 1}
+                </a>
+              </li>
             )
           }
         )}
       </ol>
 
-      <p id="bits">{bits && `0b${bits?.join('')}`}</p>
+      <section id="question">
+        {(() => {
+          const { entry } = getChoice(
+            {
+              idx: currentIndex,
+              choice: progress?.[currentIndex],
+            }
+          )
+          if(entry.question) {
+            return entry.question
+          } else if(Array.isArray(entry)) {
+            return entry.join(' or ')
+          } else {
+            return 'Unknown type of entry.'
+          }
+        })()}
+      </section>
+      <Tooltip id="app-tooltip"/>
     </main>
   )
 }
